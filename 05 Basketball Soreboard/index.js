4b12aea20356f069a210eb6de8ac28f6f9184ebd
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2022-07-28 16:34:41 +0430

    Add files via upload

---
+++ 05 Basketball Soreboard/index.js
@@ -0,0 +1,34 @@
+let questCount = 0
+
+function add1Q() {
+    questCount += 1
+    document.getElementById("quest-counter").textContent = questCount
+}
+
+function add2Q() {
+    questCount += 2
+    document.getElementById("quest-counter").textContent = questCount
+}
+
+function add3Q() {
+    questCount += 3
+    document.getElementById("quest-counter").textContent = questCount
+}
+
+
+let homeCount = 0
+
+function add1H() {
+    homeCount += 1
+    document.getElementById("home-counter").textContent = homeCount
+}
+
+function add2H() {
+    homeCount += 2
+    document.getElementById("home-counter").textContent = homeCount
+}
+
+function add3H() {
+    homeCount += 3
+    document.getElementById("home-counter").textContent = homeCount
+}
