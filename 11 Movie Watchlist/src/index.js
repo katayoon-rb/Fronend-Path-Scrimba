519e70a09df40a60934305cbf2f1041c53249f0a
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2022-09-08 15:14:10 +0430

    Add files via upload

---
+++ 11 Movie Watchlist/src/index.js
@@ -0,0 +1,86 @@
+const inputBar = document.getElementById("search-input")
+let filmInfo = {}
+const startMsg = document.getElementById("start")
+const notFound = document.getElementById("not-found")
+const results = document.getElementById("results")
+const addOrRemove = document.getElementById("add-watch")
+
+let keyArr = []
+for (var key in localStorage) {
+    keyArr.push(key)
+}
+
+
+
+document.getElementById("search-button").addEventListener("click", () => {
+    let input = inputBar.value.split(" ").join("+");
+    fetch(`http://www.omdbapi.com/?apikey=ec7cbf3b&t=${input}`)
+        .then(res => res.json())
+        .then(data => {
+            if (data.Response === "True") {
+                Object.assign(filmInfo, data)
+                startMsg.classList.add("hidden")
+                notFound.classList.add("hidden")
+                results.innerHTML = `
+                    <div class="results">
+                        <img src=${filmInfo.Poster} class="poster">
+                        <div class="texts">
+                            <div class="results-lines first-results-lines">
+                                <h2>${filmInfo.Title} ${filmInfo.Year}</h2>
+                                <img src="images/star-vector.png">
+                                <p>${filmInfo.imdbRating}</p>
+                            </div>
+                            <div class="results-lines second-results-lines">
+                                <p>${filmInfo.Runtime}</p>
+                                <p>${filmInfo.Genre}</p>
+                                <div id="add-watch">
+                                    ${keyArr.includes(`${filmInfo.Title}`) ? `
+                                        <button id="remove" onclick="removeFromWatchList()">
+                                            <img src="images/remove-vector.png">  
+                                            Remove
+                                        </button>
+                                    ` : `
+                                        <button id="add" onclick="addToWatchList()">
+                                            <img src="images/add-vector.png">  
+                                            WatchList
+                                        </button>`}
+                                </div>
+                            </div>
+                            <div class="results-lines results-desc">
+                                <p>${filmInfo.Plot}</p>
+                            </div>
+                        </div>
+                    </div>
+                `
+            } else {
+                results.innerHTML = ""
+                notFound.classList.remove("hidden")
+                startMsg.classList.add("hidden")
+            } 
+            
+            inputBar.value = ""
+        })
+})
+
+
+
+function addToWatchList() {
+    for (item in keyArr){
+        localStorage.setItem(`${filmInfo.Title}`, JSON.stringify(filmInfo))
+    }
+    document.getElementById("add-watch").innerHTML = `
+        <button id="remove" onclick="removeFromWatchList()">
+            <img src="images/remove-vector.png">
+            Remove
+        </button>`
+}
+
+function removeFromWatchList() {
+    localStorage.removeItem(`${filmInfo.Title}`)
+    document.getElementById("add-watch").innerHTML = `
+        <button id="add" onclick="addToWatchList()">
+            <img src="images/add-vector.png">  
+            WatchList
+        </button>`
+}
+
---
+++ 11 Movie Watchlist/src/watchlist.js
@@ -0,0 +1,50 @@
+let keyArr = []
+for (var key in localStorage) {
+    keyArr.push(key)
+}
+
+let listedFilms = new Array
+let listedTitles = new Array
+
+for (let i = 0; i < keyArr.length - 6; i++) {
+
+    listedFilms.push(JSON.parse(localStorage.getItem(`${keyArr[i]}`)))
+    listedTitles.push(listedFilms[i].Title)
+
+    document.getElementById("WatchList-results").innerHTML += `
+        <div class="results" id=${listedFilms[i].Title}>
+            <img src=${listedFilms[i].Poster} class="poster">
+            <div class="texts">
+                <div class="results-lines first-results-lines">
+                    <h2>${listedFilms[i].Title} ${listedFilms[i].Year}</h2>
+                    <p><img src="images/star-vector.png">${listedFilms[i].imdbRating}</p>
+                </div>
+                <div class="results-lines second-results-lines">
+                    <p>${listedFilms[i].Runtime}</p>
+                    <p>${listedFilms[i].Genre}</p>
+                    <button id="remove-watch">
+                        <button id="remove" onclick="removeFromWatchList(${i})">
+                            <img src="images/remove-vector.png">  
+                            Remove
+                        </button>
+                    </button>
+                </div>
+                <div class="results-lines results-desc">
+                    <p>${listedFilms[i].Plot}</p>
+                </div>
+            </div>
+        </div>
+    `
+}
+
+function removeFromWatchList(i) {
+    document.getElementById(listedTitles[i]).remove()
+    localStorage.removeItem(listedTitles[i])
+}
+
+
+if (keyArr.length > 6) {
+    document.getElementById("not-found-wl").classList.add("hidden")
+} else {
+    document.getElementById("not-found-wl").classList.remove("hidden")
+}
